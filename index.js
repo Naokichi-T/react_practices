5ad92d43df1634a3a0a9c07d5cbca7678825b073
Naokichi <38410014+Naokichi-T@users.noreply.github.com>
2020-12-31 22:40:49 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(
-  <App title="App" message="This is App Component!" />,
-  document.getElementById("root")
-);
