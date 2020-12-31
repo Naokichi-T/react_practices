48ecc78bd6b7b468562b7ad632dbc23a8c8b6685
Naokichi <38410014+Naokichi-T@users.noreply.github.com>
2020-12-31 21:29:03 +0900

    3-9

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+      <p>This is sample component.</p>
+      <p>これはサンプルのコンポーネントです。</p>
+    </div>
+  );
+}
+
+export default App;
